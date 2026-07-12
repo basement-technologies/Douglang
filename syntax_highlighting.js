674750b2950e9caf93d80ff3f723aabccc81cb0c
sylvieisnton <sylvieisnton@users.noreply.github.com>
2026-07-12 21:30:01 +0000

    deploy: 19be72a6edc14def86d7532e3e0a8c2d97a68dcb

---
+++ syntax_highlighting.js
@@ -0,0 +1,18 @@
+hljs.registerLanguage('douglang', function(hljs) {
+  return {
+    name: 'Douglang',
+    contains: [
+      { className: 'keyword', begin: /\+set|-set|\*set|\/set|%set|set|tts|prediction|Believers|Doubters|win|loop|gouD|Rigged/ },
+      { className: 'keyword', begin: /(Doug)+/ },
+      { className: 'keyword', begin: /Bald/ },
+      { className: 'number', begin: /(\d+(\.\d+)?|\.\d+)/, relevance: 0 },
+      hljs.QUOTE_STRING_MODE,
+      { className: 'comment', begin: /D:/, end: /:D/ },
+    ]
+  };
+});
+
+// Highlight inline code blocks
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('code.language-douglang').forEach((el) => hljs.highlightElement(el));
+});
